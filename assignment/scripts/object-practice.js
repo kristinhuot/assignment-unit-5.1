9801627e03e9e54b3c4bb9718d06a028a9c79b83
Kristin Huot <kristin.huot@gmail.com>
2024-03-21 14:09:28 -0500

    Fixed question 2 to get the last index number dynamically

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,7 +9,11 @@ console.log('***** Object Practice *****')
   - Also give it an array 'favThreeFoods'
 */
 const me = {
-  // TODO - add properties here
+  firstName: 'Kristin',
+  lastName: 'Huot',
+  hasSiblings: true,
+  shoeCount: 12,
+  favThreeFoods: ['popcorn', 'berries', 'stir fry']
 };
 console.log('A little about me:', me);
 
@@ -20,13 +24,18 @@ console.log('A little about me:', me);
   - Console.log fullName
 */
 
+let fullName = me.firstName + " " + me.lastName;  
 
+console.log('This is my full name:', fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
+let index = me.favThreeFoods.length-1; // gives the last index item a variable name so we can input this instead of the actual index #
+console.log('This is my first favorite food:', (me.favThreeFoods[0]));
+console.log('This is my last favorite food:', (me.favThreeFoods[index])); // shortened way to pull the last index item
+console.log('This is the longer version:', me.favThreeFoods[me.favThreeFoods.length-1]); // longer way to pull the last index item
 
 
 /* 4. Change a property of an existing object.
@@ -36,6 +45,9 @@ console.log('A little about me:', me);
   - Console.log your updated shoe count. 
 */
 
+console.log('This is my current estimated shoe count:', me.shoeCount);
+me.shoeCount = me.shoeCount + 1;
+console.log('This is my new shoe count:', me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +55,6 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'mauve'; 
+console.log(me);
